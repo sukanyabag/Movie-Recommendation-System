9d4e5a85d60cf611026f63ca7d47aeb26b2fce2e
Sukanya Bag <sukannya472@gmail.com>
2021-09-04 21:36:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+import flask
+from flask import Flask,request,jsonify
+from flask_restful import Resource, Api
+from flask_cors import CORS
+import difflib
+import pandas as pd
+#nlp text processing
+from sklearn.feature_extraction.text import CountVectorizer
+#ml model
+from sklearn.metrics.pairwise import cosine_similarity
+
+app = flask.Flask(__name__, template_folder='templates')
+api = Api(app)
+#CORS(app) 
+
+
+df2 = pd.read_csv('./model/tmdb.csv')
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(df2['soup'])
+
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+
+df2 = df2.reset_index()
+indices = pd.Series(df2.index, index=df2['title'])
+all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+
+@app.route('/')
+def get_recommendations(title):
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    tit = df2['title'].iloc[movie_indices]
+    dat = df2['release_date'].iloc[movie_indices]
+    return_df = pd.DataFrame(columns=['Title','Year'])
+    return_df['Title'] = tit
+    return_df['Year'] = dat
+    return return_df
+
+# Set up the main api route with backend flask api requests - get and post
+@app.route('/', methods=['GET', 'POST'])
+
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('index.html'))
+            
+    if flask.request.method == 'POST':
+        m_name = flask.request.form['movie_name']
+        m_name = m_name.title()
+#        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
+        if m_name not in all_titles:
+            return(flask.render_template('negative.html',name=m_name))
+        else:
+            result_final = get_recommendations(m_name)
+            names = []
+            dates = []
+            for i in range(len(result_final)):
+                names.append(result_final.iloc[i][0])
+                dates.append(result_final.iloc[i][1])
+
+            return flask.render_template('positive.html',movie_names=names,movie_date=dates,search_name=m_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
